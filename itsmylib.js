791983639c9fc6ddca8fb595401bc1b4a95fc387
DaiwikKanneedi <daiwik.saatvik@gmail.com>
2021-04-01 19:48:11 +0530

    git call me BOOOOOSSSSSSS

---
+++ itsmylib.js
@@ -0,0 +1,18 @@
+class itsmylib {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
+  //don't mine DNSK its fort fun do not confuse its just time passs 
